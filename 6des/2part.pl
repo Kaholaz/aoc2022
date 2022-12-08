39e63e530643076609b49ad7273d90f370bb0d92
Kaholaz <nim.nimern@gmail.com>
2022-12-08 16:13:05 +0100

    Formatted using perltidy

--- 6des/2part.pl
+++ 6des/2part.pl
@@ -7,7 +7,7 @@ sub has_duplicates {
     my $string = shift;
 
     my %seen;
-    foreach (split //, $string) {
+    foreach ( split //, $string ) {
         $seen{$_} = 1;
     }
 
@@ -15,15 +15,15 @@ sub has_duplicates {
 }
 
 sub first_index_with_duplicates {
-    my ($string, $length) = @_;
-    for (my $i = 0; $i < (length $string) - 4; ++$i) {
-        my $s = substr ($string, $i, $length);
+    my ( $string, $length ) = @_;
+    for ( my $i = 0 ; $i < ( length $string ) - 4 ; ++$i ) {
+        my $s = substr( $string, $i, $length );
         return $i unless has_duplicates $s;
     }
 }
 
 my $length = 14;
-my $s = <>;
-my $i = first_index_with_duplicates ($s, $length);
+my $s      = <>;
+my $i      = first_index_with_duplicates( $s, $length );
 
 print $i + $length, "\n";
